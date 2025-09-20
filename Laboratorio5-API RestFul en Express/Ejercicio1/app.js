45aeb24a25e1168aaf35b947d7b88ebdc2e59273
ItzDaniell <juan.rodriguez.o@tecsup.edu.pe>
2025-09-20 09:22:01 -0500

    feat: Activity 1 - API RestFul using json file for database and external dependencies

---
+++ Laboratorio5-API RestFul en Express/Ejercicio1/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+
+const ticketRoutes = require("./routes/ticket.routes");
+const notificationRoutes = require("./routes/notification.routes");
+
+// Middleware
+app.use(express.json()); // Enviar y recibir JSON
+app.use(cors()); // Permitir solicitudes desde cualquier origen
+app.use(morgan("dev")); // Detalles de la petición en consola
+
+// Mensaje de prueba
+app.get('/', (req, res) => {
+    res.send('API RestFul con Express');
+})
+
+app.use("/tickets", ticketRoutes);
+app.use("/notifications", notificationRoutes);
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+})
